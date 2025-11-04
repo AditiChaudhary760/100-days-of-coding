20702a31a8adad59660149a81cdab98b1f584b05
Aditi Chaudhary <aditichaudhary760@gmail.com>
2025-11-04 19:20:35 +0530

    day 4 ques 1
    day 4 ques 1

---
+++ 7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+    struct student {
+        char name[100];
+        float marks[3];
+        float average;
+
+    };
+    int main() {
+     struct student s;
+     int i, total = 0;
+
+     printf("enter student name: ");
+     scanf("%s", s.name);
+    printf("enter marks in 5 subjects");
+for(i = 0; i<5; i++) {
+    printf("subject %d:", i+1);
+}   
+    }
