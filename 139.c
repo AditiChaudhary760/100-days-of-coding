65d3967b481055dbd2851ad932ec8519278e27ed
Aditi Chaudhary <aditichaudhary760@gmail.com>
2025-11-25 14:44:15 +0530

    day 89

---
+++ 139.c
@@ -0,0 +1,12 @@
+/* Q138: Print all enum names and integer values using a loop.*/
+
+#include<stdio.h>
+
+enum Color { RED, YELLOW, GREEN };
+
+int main() {
+    printf("RED = %d\n", RED);
+    printf("YELLOW = %d\n", YELLOW);
+    printf("GREEN = %d\n", GREEN);
+    return 0;
+}
