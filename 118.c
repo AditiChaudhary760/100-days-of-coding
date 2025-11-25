e9e4c6e978718e753061f245226de8c737edaef1
Aditi Chaudhary <aditichaudhary760@gmail.com>
2025-11-25 14:24:53 +0530

    day 68

---
+++ 118.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements (from 0 to %d with one missing):\n", n, n);
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    int expectedSum = n * (n + 1) / 2;
+    int actualSum = 0;
+    
+    for(int i = 0; i < n; i++){
+        actualSum += arr[i];
+    }
+
+    int missingNumber = expectedSum - actualSum;
+
+    printf("Missing number: %d\n", missingNumber);
+   return 0;
+   
+}
